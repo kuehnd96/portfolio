aca04ce4a53f38c9fb451d17898e323257a6918c
David Kuehn <kuehnd96@gmail.com>
2025-03-08 06:28:43 -0600

    180: Step 4 of authentication creation

---
+++ src/DavidKuehn.Portfolio.WebApi/Models/ResponseObject.cs
@@ -0,0 +1,11 @@
+﻿namespace DavidKuehn.Portfolio.WebApi.Models
+{
+    [Obsolete("Came with authentication instructions; Not sure I need this.")]
+    public class ResponseObject<TEntity> where TEntity : class
+    {
+        public IEnumerable<TEntity>? Records { get; set; }
+        public TEntity? Record { get; set; }
+        public string? Message { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
