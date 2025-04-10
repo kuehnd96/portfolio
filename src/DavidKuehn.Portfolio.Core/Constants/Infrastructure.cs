cff3163d171ffb8a273521881bce50dc73dd38c6
David Kuehn <kuehnd96@gmail.com>
2025-04-09 21:12:36 -0500

    180: Step 10 of authentication instructions

--- src/DavidKuehn.Portfolio.Core/Constants/Infrastructure.cs
+++ src/DavidKuehn.Portfolio.Core/Constants/Infrastructure.cs
@@ -14,5 +14,15 @@
         /// Name of the environment variable that contains the connection string to the career database.
         /// </summary>
         public static string CareerDatabaseConnectionStringName = "CAREER_DATABASE_CONNECTION_STRING";
+        
+        /// <summary>
+        /// Name of the environment variable that contains the number of minutes a JWT is alive for.
+        /// </summary>
+        public static string JwtExpiryInMinutesName = "PORTFOLIO_JWT_EXPIRY_MINUTES";
+        
+        /// <summary>
+        /// Name of the environment variable that contains the secret key for JWT signing.
+        /// </summary>
+        public static string JwtSecretKey = "PORTFOLIO_JWT_SECRET_KEY";
     }
 }
--- src/DavidKuehn.Portfolio.WebApi/Program.cs
+++ src/DavidKuehn.Portfolio.WebApi/Program.cs
@@ -1,8 +1,14 @@
 using DavidKuehn.Portfolio.Core.Constants;
 using DavidKuehn.Portfolio.WebApi.Models.Career;
 using DavidKuehn.Portfolio.WebApi.Models.IdentityProvider;
+using DavidKuehn.Portfolio.WebApi.Services.IdentityProvider.Core_RBS_Tokens.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http.Json;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,6 +37,69 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     // Use the EF Core for Creating, Managing Users and Roles
     .AddEntityFrameworkStores<SecurityDbContext>().AddDefaultTokenProviders();
 
+#region The CORS
+builder.Services.AddCors(options => // TODO: Tweak this after tracer bullets
+{
+    options.AddPolicy("cors", (policy) =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+#endregion
+
+builder.Services.AddScoped<SecurityServices>();
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("AdminPolicy", policy =>
+    {
+        policy.RequireRole("Admin");
+    })
+    .AddPolicy("ReaderPolicy", policy =>
+    {
+        policy.RequireRole("Reader");
+    });
+
+#region Token Validation
+// Read the Secret Key from the appsettings.json
+var secretKeyString = Environment.GetEnvironmentVariable(Infrastructure.JwtSecretKey);
+if (string.IsNullOrEmpty(secretKeyString))
+{
+    throw new InvalidOperationException("Secret key is not configured properly.");
+}
+byte[] secretKey = Convert.FromBase64String(secretKeyString);
+
+// set the Authentication Scheme
+builder.Services.AddAuthentication(options => 
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+       
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = null,
+            ValidAudience = null,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKeyString))
+        };
+    });
+#endregion
+
+#region The JSON Serialization
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.PropertyNamingPolicy = null; // Use Pascal case
+    options.SerializerOptions.DictionaryKeyPolicy = null; // Use Pascal case
+    options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
+#endregion
+
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
@@ -43,6 +112,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("cors");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
