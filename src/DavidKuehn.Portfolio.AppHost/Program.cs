2ee03173cc332b2f310d143ddfcade78e30bb6cd
David Kuehn <kuehnd96@gmail.com>
2025-08-31 22:14:28 -0500

    181: Set up local API key

--- src/DavidKuehn.Portfolio.AppHost/Program.cs
+++ src/DavidKuehn.Portfolio.AppHost/Program.cs
@@ -1,5 +1,16 @@
+using Aspire.Hosting;
+
+const string ApiKeyEnvironmentName = "PORTFOLIO_API_KEY";
+
 var builder = DistributedApplication.CreateBuilder(args);
 
+#if DEBUG
+var apiKey = Environment.GetEnvironmentVariable(ApiKeyEnvironmentName);
+
+builder.AddProject<Projects.DavidKuehn_Portfolio_WebApi>("portfolio-webapi")
+    .WithEnvironment(ApiKeyEnvironmentName, apiKey);
+#else
 builder.AddProject<Projects.DavidKuehn_Portfolio_WebApi>("portfolio-webapi");
+#endif
 
 builder.Build().Run();
