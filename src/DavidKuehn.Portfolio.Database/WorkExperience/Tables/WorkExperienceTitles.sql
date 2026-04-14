c13361bc9567deebe14811e22307ac7a4fb49069
David Kuehn <kuehnd96@gmail.com>
2026-04-14 14:00:09 -0500

    202: Create database tables for work experience

---
+++ src/DavidKuehn.Portfolio.Database/WorkExperience/Tables/WorkExperienceTitles.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[WorkExperienceTitle]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(), 
+    [JobId] UNIQUEIDENTIFIER NOT NULL,
+    [Title] VARCHAR(50) NOT NULL, 
+    [StartYear] INT NOT NULL, 
+    [EndYear] INT NOT NULL, 
+    [Details] NVARCHAR(100) NOT NULL, 
+    [BulletPoints] VARCHAR(MAX) NOT NULL,
+    CONSTRAINT [FK_WorkExperienceTitle_Jobs] FOREIGN KEY ([JobId]) REFERENCES [dbo].[WorkExperienceJobs]([Id])
+)
+
