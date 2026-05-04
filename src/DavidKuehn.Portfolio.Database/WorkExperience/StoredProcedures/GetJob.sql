ee13215c83fee4b2572ec11f8c10e93d6b3e6fa8
David Kuehn <kuehnd96@gmail.com>
2026-05-03 21:24:32 -0500

    203 205: Create sprocs for getting job data

---
+++ src/DavidKuehn.Portfolio.Database/WorkExperience/StoredProcedures/GetJob.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[GetAllJobs]
+	@JobId INT
+AS
+ SELECT
+		j.[Id],
+		j.[StartYear],
+		j.[EndYear],
+		j.[Company],
+		j.[CurrentCompanyName],
+		j.[Type],
+		t.[Id] AS [TitleId],
+		t.[Title],
+		t.[StartYear] AS [TitleStartYear],
+		t.[EndYear] AS [TitleEndYear],
+		t.[Details],
+		t.[BulletPoints],
+		js.[SkillId],
+		s.[Name] AS [SkillName],
+		s.[Type] AS [SkillType]
+	FROM [dbo].[WorkExperienceJobs] j
+	LEFT JOIN [dbo].[WorkExperienceTitle] t ON t.[JobId] = j.[Id]
+	LEFT JOIN [dbo].[WorkExperienceJobSkill] js ON js.[JobId] = j.[Id]
+	LEFT JOIN [dbo].[WorkExperienceSkill] s ON s.[Id] = js.[SkillId]
+	WHERE j.Id = @JobId
---
+++ src/DavidKuehn.Portfolio.Database/WorkExperience/StoredProcedures/GetJobList.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[GetJobList]
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT
+        j.[StartYear],
+        j.[EndYear],
+        j.[Company],
+        j.[CurrentCompanyName],
+        lt.[Title]
+    FROM [dbo].[WorkExperienceJobs] j
+    OUTER APPLY
+    (
+        SELECT TOP (1)
+            t.[Title]
+        FROM [dbo].[WorkExperienceJobTitles] t
+        WHERE t.[WorkExperienceJobId] = j.[Id]
+        ORDER BY t.[StartYear] DESC
+    ) lt
+    ORDER BY j.[StartYear] DESC, j.[EndYear] DESC;
+END
